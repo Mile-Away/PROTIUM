33a86932308272e8e1338266a4894908ee42b01c
Harvey Que <Q-Query@outlook.com>
2025-05-03 22:45:43 +0800

    Refactor: Update container names in docker-compose and add new VSCode settings and extensions

--- service/src/main.go
+++ service/src/main.go
@@ -2,10 +2,26 @@ package main
 
 import (
 	"fmt"
+	"net/http"
+
+	"github.com/google/uuid"
 )
 
 func main() {
-
 	card := "1234-5678-9012-3456"
+	card = "****-****-****-3456"
+	card = uuid.New().String()
 	fmt.Println("Hello, World!", card)
+
+	http.ListenAndServe(":8080", nil)
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello, World!")
+	},
+	)
+
+	http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello, World!")
+	},
+	)
 }
